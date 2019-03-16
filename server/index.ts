524ecf70f9096ab9d82eaa8afb49a74d04437460
Robert Webb <robertwebb@Roberts-Air.attlocal.net>
2019-03-16 12:00:26 -0700

    Added chess board

--- server/index.ts
+++ server/index.ts
@@ -4,13 +4,8 @@ import path from 'path';
 const port: number = parseInt(process.env.port || '', 10) || 3001;
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// define a route handler for the default home page
 app.get('/', async (req, res) => {
   // render the index template
-  const h: string = 'a' + 2;
 });
 
 // start the express server
