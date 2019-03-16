6512735b707f1eed202780193858f8b5dde6fd7c
Robert Webb <robertwebb@Roberts-Air.attlocal.net>
2019-03-16 10:40:13 -0700

    Added react

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+
+const port: number = parseInt(process.env.port || '', 10) || 4000;
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// define a route handler for the default home page
+app.get('/', async (req, res) => {
+  // render the index template
+  const h: string = 'a' + 2;
+});
+
+// start the express server
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
+
+export default app;
