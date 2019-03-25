7e9c601feb0db2dcc2abb77e7a73c0d8a39c563e
Robert Webb <robertwebb@Roberts-Air.attlocal.net>
2019-03-24 19:31:43 -0700

    refactored board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,7 +6,7 @@ import Square from './Square';
 
 export default class Board extends Component {
   public props: { squares: Piece[]; onClick: (i: number) => void };
-  public renderSquare(i, squareShade, boardPosition: string) {
+  public renderSquare(i: number, squareShade: Color, boardPosition: string) {
     return (
       <Square
         key={i}
@@ -20,33 +20,38 @@ export default class Board extends Component {
   }
 
   public render() {
-    const board = [];
-    for (let i = 0; i < 8; i++) {
-      const squareRows = [];
-      for (let j = 0; j < 8; j++) {
-        const squareShade =
-          (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))
-            ? Color.white
-            : Color.black;
-        squareRows.push(
-          this.renderSquare(
-            i * 8 + j,
-            squareShade,
-            String.fromCharCode(64 + j + 1) + (8 - i)
-          )
-        );
-      }
-      board.push(
-        <div key={i} className="board-row">
-          {squareRows}
-        </div>
-      );
-    }
+    const board = getEmptyArray().map(i => (
+      <div key={i} className="board-row">
+        {this.getSquareRows(i)}
+      </div>
+    ));
 
     return <div>{board}</div>;
   }
+
+  private getSquareRows(i: number) {
+    return getEmptyArray().map(j =>
+      this.renderSquare(
+        i * 8 + j,
+        getSquareShade(i, j),
+        String.fromCharCode(64 + j + 1) + (8 - i)
+      )
+    );
+  }
 }
 
-function isEven(num) {
+function getEmptyArray(len = 8) {
+  return Array(len)
+    .fill(0)
+    .map((_, idx) => idx);
+}
+
+function getSquareShade(i: number, j: number): Color {
+  return (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j))
+    ? Color.white
+    : Color.black;
+}
+
+function isEven(num: number) {
   return num % 2 === 0;
 }
